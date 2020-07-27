3db58a75a0069036e2196e084f33757213c53697
wellingtonlopes <ookami.seta@gmail.com>
2020-07-26 21:05:47 -0400

    Updates

---
+++ precache-manifest.fc967627f93c176a84f949a7493e33d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d4e82f262688b7861a7c4cec3ac3e2",
+    "url": "/PokeWebDex/index.html"
+  },
+  {
+    "revision": "c02f2aad6aa09328164a",
+    "url": "/PokeWebDex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5f0c8b3920530dae0ae7",
+    "url": "/PokeWebDex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c02f2aad6aa09328164a",
+    "url": "/PokeWebDex/static/js/2.ca103480.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokeWebDex/static/js/2.ca103480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f0c8b3920530dae0ae7",
+    "url": "/PokeWebDex/static/js/main.cb05a0c8.chunk.js"
+  },
+  {
+    "revision": "96e66e0aff83bad8910a",
+    "url": "/PokeWebDex/static/js/runtime-main.27904883.js"
+  }
+]);
