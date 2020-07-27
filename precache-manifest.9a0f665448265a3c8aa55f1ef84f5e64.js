7769ee2ecc520f61e7b48385ea947607a16aa78f
wellingtonlopes <ookami.seta@gmail.com>
2020-07-26 22:55:39 -0400

    Updates

---
+++ precache-manifest.9a0f665448265a3c8aa55f1ef84f5e64.js
@@ -0,0 +1,45 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2d88ff6971b357bb81045f029379fa7",
+    "url": "/PokeWebDex/index.html"
+  },
+  {
+    "revision": "f8a67e3852a38bc3b663",
+    "url": "/PokeWebDex/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fe438ecf728f2ddb0606",
+    "url": "/PokeWebDex/static/css/main.72e75b78.chunk.css"
+  },
+  {
+    "revision": "f8a67e3852a38bc3b663",
+    "url": "/PokeWebDex/static/js/2.520688c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokeWebDex/static/js/2.520688c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe438ecf728f2ddb0606",
+    "url": "/PokeWebDex/static/js/main.fa1a0543.chunk.js"
+  },
+  {
+    "revision": "96e66e0aff83bad8910a",
+    "url": "/PokeWebDex/static/js/runtime-main.27904883.js"
+  },
+  {
+    "url": "/PokeWebDex/static/media/6e5393a64ca23c455e8568d409e82da6.05fa7200.woff"
+  },
+  {
+    "url": "/PokeWebDex/static/media/6e5393a64ca23c455e8568d409e82da6.2ba55678.woff2"
+  },
+  {
+    "url": "/PokeWebDex/static/media/6e5393a64ca23c455e8568d409e82da6.385cb7e3.svg"
+  },
+  {
+    "url": "/PokeWebDex/static/media/6e5393a64ca23c455e8568d409e82da6.6e5393a6.ttf"
+  },
+  {
+    "url": "/PokeWebDex/static/media/6e5393a64ca23c455e8568d409e82da6.f223d1f8.eot"
+  }
+]);
